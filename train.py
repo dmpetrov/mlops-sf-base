7c9274c470db62f2dddcef4921abd54ac25ca84d
Dmitry Petrov <dmitry.petrov@nevesomo.com>
2021-03-30 08:40:01 -0700

    save checkpoint

--- train.py
+++ train.py
@@ -3,22 +3,14 @@ import datetime
 import yaml
 import json
 import time
-#import neptune
-#from neptunecontrib.monitoring.keras import NeptuneMonitor
-#import wandb
-#from wandb.keras import WandbCallback
 from mymodel import create_model
 import os
-#from elog.keras import ElogCallback
 from tensorflow import keras
 from tensorflow.keras.callbacks import ModelCheckpoint, Callback
 from dvc.api import make_checkpoint
 
 import dvclive
 from dvclive.keras import DvcLiveCallback
-
-#dvclive.init()
-#wandb.init(project="mnist")
 
 weights_file = "model.h5"
 summary = "summary.json"
@@ -27,10 +19,7 @@ params = yaml.safe_load(open('params.yaml'))
 epochs = params['epochs']
 log_file = params['log_file']
 dropout = params['dropout']
-#dvc_logs_dir = params['dvc_logs_dir']
 lr = params['lr']
-
-#logs_subdir = 'logs'
 
 class MyCallback(Callback):
     def __init__(self, file):
@@ -39,9 +28,6 @@ class MyCallback(Callback):
         self.model.save(self.file)
         json.dump(logs, open(summary, 'w'))
         make_checkpoint()
-
-#neptune.init('dmpetrov/sandbox')
-#neptune.create_experiment(name='exp1', params=params)
 
 mnist = tf.keras.datasets.mnist
 
@@ -57,11 +43,6 @@ model.compile(optimizer=opt,
 if os.path.exists(weights_file):
     model.load_weights(weights_file)
 
-#log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-#tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-#csv_logger = tf.keras.callbacks.CSVLogger(log_file)
-
 start_real = time.time()
 start_process = time.process_time()
 history = model.fit(x=x_train,
@@ -69,14 +50,7 @@ history = model.fit(x=x_train,
                     epochs=epochs,
                     validation_data=(x_test, y_test),
                     callbacks=[
-                        #csv_logger,
-                        #tensorboard_callback
-                        #, NeptuneMonitor()
-                        #, WandbCallback()
-                        #, ElogCallback()
-                        #, ModelCheckpoint(weights_file, monitor='loss',
-                        #                    mode='auto', period=1)
-                        ##, MyCallback(weights_file)
+                        MyCallback(weights_file),
                         DvcLiveCallback()
                     ])
 end_real = time.time()
